80ea9838836e9e23763dc65eb6bcd4188bbee461
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2022-12-15 11:25:41 -0500

    try use @GetMapping

--- src/com/mmtrios/springdemo/controller/CustomerController.java
+++ src/com/mmtrios/springdemo/controller/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mmtrios.springdemo.dao.CustomerDAO;
@@ -17,7 +19,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
 		// get customers from dao, and add it to theModel
